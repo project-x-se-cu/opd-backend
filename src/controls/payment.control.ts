6b0a60d660bcc116ed74a4976c15582d29179e37
Pattadon Prongpreecha <pattadon.pro@gmail.com>
2022-04-21 22:52:31 +0700

    Create payment

--- src/controls/payment.control.ts
+++ src/controls/payment.control.ts
@@ -7,6 +7,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { PaymentDto } from 'src/dtos/payment-dto';
 import { RecieptDto } from 'src/dtos/reciept-dto';
 import { InvoiceStatus } from 'src/enums/invoice-status.enum';
+import { TwoCTwoPProxy } from 'src/proxies/2c2p.proxy';
 import { InvoiceService } from 'src/services/invoice.service';
 import { RecieptService } from 'src/services/reciept.service';
 
@@ -15,8 +16,16 @@ import { RecieptService } from 'src/services/reciept.service';
 export class PaymentControl {
   constructor(
     private readonly invoiceService: InvoiceService,
-    private readonly recieptService: RecieptService
+    private readonly recieptService: RecieptService,
+    private readonly twoCtwoPProxy: TwoCTwoPProxy
   ) { }
+
+  @Post('payments/request')
+  async requestPayment(@Body() requestPayment: PaymentDto) {
+    await this.invoiceService.findByRefId(requestPayment.refId);
+    this.twoCtwoPProxy.createPayment();
+    return { redirectUrl: 'https://2c2p.com' }
+  }
 
   @Post('payments')
   async createPayment(@Body() createPaymentRequest: PaymentDto) {
